166b7388de8d9d1aa72fc7b88a61fb90484ece68
Mayank Dhanagar <mayankdhangar1007@gmail.com>
2026-07-31 08:53:13 +0530

    valid sukudo.cpp

--- Arrays/valid suduko.cpp
+++ Arrays/valid suduko.cpp
@@ -1,46 +1,25 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int n =board.size();
-        for(int row =0;row<n;row++){
-             unordered_set<char>st;
-        
-             for(int i =0;i<n;i++){
-                if(board[row][i]=='.')continue;
-                if(st.find(board[row][i])!=st.end()){
-                    return false;
-                    
-                }
-                st.insert(board[row][i]);
-             }
-            
-        }
-        for(int col=0;col<n;col++){
-            unordered_set<char>st;
-            for(int j=0;j<n;j++){
-                if(board[j][col]=='.')continue;
-                if(st.find(board[j][col])!=st.end()){
+        vector<unordered_set<char>>rows(9);
+        vector<unordered_set<char>>cols(9);
+        vector<unordered_set<char>>boxes(9);
+        for(int row=0;row<9;row++){
+            for(int col=0;col<9;col++){
+                char num=board[row][col];
+                if(num=='.')continue;
+                int boxIndex = (row/3)*3+(col/3);
+                if((rows[row].find(num)!=rows[row].end())||
+                (cols[col].find(num)!=cols[col].end())||
+                (boxes[boxIndex].find(num)!=boxes[boxIndex].end())){
                     return false;
                 }
-                st.insert(board[j][col]);
-            }
-        }
-        for(int row=0;row<n;row+=3){
-            for(int col=0;col<n;col+=3){
-                unordered_set<char>st;
-                for(int i =row;i<row+3;i++){
-                    for(int j=col;j<col+3;j++){
-                        if(board[i][j]=='.')continue;
-                        if(st.find(board[i][j])!=st.end()){
-                            return false;
-                        }
-                        st.insert(board[i][j]);
+                rows[row].insert(num);
+                cols[col].insert(num);
+                boxes[boxIndex].insert(num);
 
-                    }
-                }
             }
         }
-            
-     return true; 
+     return true;   
     }
 };
