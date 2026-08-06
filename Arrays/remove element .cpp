9b0738bb10d25760600b66127eaf2c19f2f892b9
Mayank Dhanagar <mayankdhangar1007@gmail.com>
2026-08-07 00:09:19 +0530

    remove element .cpp

--- Arrays/remove element .cpp
+++ Arrays/remove element .cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        vector<int>temp;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=val){
-                temp.push_back(nums[i]);
+        int write=0;
+        for(int read=0;read<nums.size();read++){
+            if(nums[read] !=val){
+                nums[write] = nums[read];
+                write++;
+                
             }
         }
-        for(int i = 0; i < temp.size(); i++){
-
-        nums[i] = temp[i];
-        };
-        return temp.size();
+        return write;
     }
 };
