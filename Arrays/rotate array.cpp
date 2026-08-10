8e43f84a025e09f2af16b5a79e1f0a8c3fbdc5b5
Mayank Dhanagar <mayankdhangar1007@gmail.com>
2026-08-10 08:29:44 +0530

    rotate array .cpp

--- Arrays/rotate array.cpp
+++ Arrays/rotate array.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        if(n == 0) return;
+        int n=nums.size();
+        k=k%n;
+        reverse(nums.begin(),nums.end());
 
-        k = k % n;
-
-        vector<int> temp;
-
-        for(int i = n-k; i < n; i++) {
-            temp.push_back(nums[i]);
-        }
-
-        for(int i = 0; i < n-k; i++) {
-            temp.push_back(nums[i]);
-        }
-
-        for(int i = 0; i < temp.size(); i++) {
-            nums[i] = temp[i];
-        }
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
