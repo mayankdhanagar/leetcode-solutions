80562bc959121751499b8543418d11ac72cb85a5
Mayank Dhanagar <mayankdhangar1007@gmail.com>
2026-08-16 12:06:26 +0530

    best time to buy.cpp

---
+++ Arrays/best time to buy.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+
+        for(int i = 0; i < prices.size(); i++){
+
+            for(int j = i+1; j < prices.size(); j++){
+
+                int profit = prices[j] - prices[i];
+
+                maxProfit = max(maxProfit, profit);
+            }
+        }
+
+        return maxProfit;
+    }
+};
